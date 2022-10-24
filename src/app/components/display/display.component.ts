02be5f0a9cf0ef6d1a9d4d66db798a3b0d0ca8cb
Christopher Reineborn <christopher@zylion.se>
2022-10-24 23:35:33 +0200

    Styling

--- src/app/components/display/display.component.ts
+++ src/app/components/display/display.component.ts
@@ -7,8 +7,8 @@ import { FormService } from 'src/app/services/form.service';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-  public from: string[] = []
-  public to: string[] = []
+  public from: string[] = ["Santa Claus"]
+  public to: string[] = ["Mrs Claus"]
 
   constructor(private formService: FormService) { }
 
@@ -18,32 +18,27 @@ export class DisplayComponent implements OnInit {
       this.to = [];
       let from = [...names];
       let to = [...names];
+      
 
       for(let i = 0; i < from.length; i=0){
         let fromIndex = Math.floor(Math.random() * from.length)
         let toIndex = Math.floor(Math.random() * to.length)
 
         if(!(from[fromIndex] == to[toIndex])){
-
           this.from.push(from[fromIndex]);
           this.to.push(to[toIndex]);
 
           from.splice(fromIndex, 1);
           to.splice(toIndex, 1);
+        }
 
+        if(from.length == 1  && to.length == 1 && to[0] == from[0]){
+          this.from = [];
+          this.to = [];
+          from = [...names];
+          to = [...names];
         }
       }
     })
   }
-
-  private shuffle(array: string[]): string[] {
-      for (var i = array.length - 1; i > 0; --i) {
-          var j = Math.floor(Math.random() * (i + 1));
-          var temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-      }
-      return array;
-  }
-
 }
