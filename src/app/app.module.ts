b216ca9a6488c8cacda87146722d7eea051176b8
Christopher Reineborn <christopher@zylion.se>
2022-10-24 14:54:40 +0200

    Small bug fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FromComponent } from './components/from/from.component';
-import { ToComponent } from './components/to/to.component';
 import { DisplayComponent } from './components/display/display.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    FromComponent,
-    ToComponent,
     DisplayComponent
   ],
   imports: [
